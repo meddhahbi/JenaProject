01d05fd99e1c8f4d56d0250da39eb6fec6cd4091
chaouch5 <moahmedkhalil.chaouch@esprit.tn>
2023-10-31 23:27:12 +0100

    Condidature+Reponse working

--- src/main/java/pione/jenaProject/Controller/ReponseController.java
+++ src/main/java/pione/jenaProject/Controller/ReponseController.java
@@ -4,10 +4,7 @@ import org.apache.jena.query.*;
 import org.apache.jena.rdf.model.RDFNode;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -20,15 +17,23 @@ import java.util.Map;
 public class ReponseController {
 
     @GetMapping
-    public ResponseEntity<Object> performQuery() {
-
-
-
-        String queryString = " PREFIX : <http://www.semanticweb.org/wassim/ontologies/2023/9/untitled-ontology-8#>\n" +
-                " SELECT  ?reponseId ?reponse\n" +
+    public ResponseEntity<Object> performSearch(@RequestParam("searchTerm") String searchTerm) {
+        String queryString = "PREFIX : <http://www.semanticweb.org/wassim/ontologies/2023/9/untitled-ontology-8#>\n" +
+                "SELECT ?reponseId ?reponse ?user_id ?username ?condidature_id ?email ?lastName ?firstName\n" +
                 "WHERE {\n" +
                 "   ?Reponse :reponseId ?reponseId .\n" +
                 "   ?Reponse :reponse ?reponse .\n" +
+                "   ?Reponse :responseUser ?user_id .\n" +
+                "   ?user_id :username ?username .\n" +
+                "   ?Reponse :repondre ?condidature_id .\n" +
+                "   ?condidature_id :email ?email .\n" +
+                "   ?condidature_id :lastName ?lastName .\n" +
+                "   ?condidature_id :firstName ?firstName .\n" +
+                "   FILTER (regex(str(?reponse), '" + searchTerm + "', 'i') || " +
+                "          regex(str(?email), '" + searchTerm + "', 'i') || " +
+                "          regex(str(?lastName), '" + searchTerm + "', 'i') || " +
+                "          regex(str(?firstName), '" + searchTerm + "', 'i') || " +
+                "          regex(str(?username), '" + searchTerm + "', 'i')) .\n" +
                 "}";
 
 
@@ -39,22 +44,28 @@ public class ReponseController {
         try (QueryExecution qexec = QueryExecutionFactory.sparqlService(serviceEndpoint, query)) {
             ResultSet results = qexec.execSelect();
 
-
-
             List<Object> queryResults = new ArrayList<>();
 
             while (results.hasNext()) {
                 QuerySolution solution = results.next();
                 RDFNode reponseId = solution.get("reponseId");
                 RDFNode reponse = solution.get("reponse");
-
-
-
-
+                RDFNode user_id = solution.get("user_id");
+                RDFNode username = solution.get("username");
+                RDFNode condidature_id = solution.get("condidature_id");
+                RDFNode email = solution.get("email");
+                RDFNode lastName = solution.get("lastName");
+                RDFNode firstName = solution.get("firstName");
 
                 Map<String, Object> resultItem = new HashMap<>();
                 resultItem.put("reponseId", reponseId.toString());
                 resultItem.put("reponse", reponse.toString());
+                resultItem.put("user_id", user_id.toString());
+                resultItem.put("username", username.toString());
+                resultItem.put("condidature_id", condidature_id.toString());
+                resultItem.put("email", email.toString());
+                resultItem.put("lastName", lastName.toString());
+                resultItem.put("firstName", firstName.toString());
 
                 queryResults.add(resultItem);
             }
