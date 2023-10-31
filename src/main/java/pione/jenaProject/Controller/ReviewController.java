8c35293b79890fdf02af1be2b108fb0db126a6fa
Mohamed Dhahbi <56275931+med850@users.noreply.github.com>
2023-10-31 17:41:56 +0100

    Solve some errors into GigController and add ReviewController

--- src/main/java/pione/jenaProject/Controller/ReviewController.java
+++ src/main/java/pione/jenaProject/Controller/ReviewController.java
@@ -18,29 +18,25 @@ import java.util.Map;
 @RestController
 @RequestMapping("/gigs")
 @CrossOrigin("*")
-public class GigController {
+public class ReviewController {
 
     @GetMapping
     public ResponseEntity<Object> performQuery() {
 
-
-
         String queryString = " PREFIX : <http://www.semanticweb.org/wassim/ontologies/2023/9/untitled-ontology-8#>\n" +
-                "  SELECT ?Gig ?gigId ?gigDescription ?gigTitle ?orders ?price ?user_id ?username ?category_id ?categoryName" +
+                "  SELECT ?Review ?ratingId ?ratingComment ?user_id ?username ?category_id ?categoryName" +
                 "  WHERE {\n" +
-                "    ?Gig a :Gig .\n" +
-                "    ?Gig :gigId ?gigId .\n" +
-                "    ?Gig :gigDescription ?gigDescription .\n" +
-                "    ?Gig :gigTitle ?gigTitle .\n" +
-                "    ?Gig :orders ?orders .\n" +
-                "    ?Gig :price ?price .\n" +
-                "    ?Gig :publier ?user_id .\n" +
+                "    ?Review a :Review .\n" +
+                "    ?Review :ratingId ?ratingId .\n" +
+                "    ?Review :ratingComment ?ratingComment .\n" +
+                "    ?Review :rate ?user_id .\n" +
                 "    ?user_id a :User .\n" +
                 "    ?user_id :username ?username .\n" +
-                "    ?Gig :categoriser ?category_id .\n" +
-                "    ?category_id a :Category .\n" +
-                "    ?category_id :categoryName ?categoryName .\n" +
+                "    ?Review :evaluer ?gig_id .\n" +
+                "    ?gig_id a :Gig .\n" +
+                "    ?gig_id :gigTitle ?gigTitle .\n" +
                 "}";
+
 
 
         String serviceEndpoint = "http://localhost:3030/ds/sparql";
@@ -51,32 +47,23 @@ public class GigController {
             ResultSet results = qexec.execSelect();
 
 
-
             List<Object> queryResults = new ArrayList<>();
 
             while (results.hasNext()) {
                 QuerySolution solution = results.next();
-                RDFNode gig = solution.get("Gig");
-                RDFNode gigId = solution.get("gigId");
-                RDFNode gigDescription = solution.get("gigDescription");
-                RDFNode gigTitle = solution.get("gigTitle");
-                RDFNode orders = solution.get("orders");
-                RDFNode price = solution.get("price");
+                RDFNode Review = solution.get("Review");
+                RDFNode ratingId = solution.get("ratingId");
+                RDFNode ratingComment = solution.get("ratingComment");
                 RDFNode username = solution.get("username");
-                RDFNode categoryName = solution.get("categoryName");
-
-
+                RDFNode gigTitle = solution.get("gigTitle");
 
 
                 Map<String, Object> resultItem = new HashMap<>();
-                resultItem.put("gig", gig.toString());
-                resultItem.put("gigId", gigId.toString());
-                resultItem.put("gigDescription", gigDescription.toString());
-                resultItem.put("gigTitle", gigTitle.toString());
-                resultItem.put("orders", orders.toString());
-                resultItem.put("price", price.toString());
+                resultItem.put("Review", Review.toString());
+                resultItem.put("ratingId", ratingId.toString());
+                resultItem.put("ratingComment", ratingComment.toString());
                 resultItem.put("username", username.toString());
-                resultItem.put("categoryName", categoryName.toString());
+                resultItem.put("gigTitle", gigTitle.toString());
                 queryResults.add(resultItem);
             }
 
@@ -87,9 +74,8 @@ public class GigController {
             return new ResponseEntity<>("Error occurred", HttpStatus.INTERNAL_SERVER_ERROR);
         }
 
-
-
     }
 
 
-}
+
+    }
