a6e4578fb3698445c99a87ea3d24967bfadbcdcc
Wassim-Aloui <61364693+Wassim-Aloui@users.noreply.github.com>
2023-10-31 23:45:26 +0100

    add blog / comments entities

--- src/main/java/pione/jenaProject/Controller/UserController.java
+++ src/main/java/pione/jenaProject/Controller/UserController.java
@@ -1,4 +1,11 @@
 package pione.jenaProject.Controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+@CrossOrigin("*")
 public class UserController {
 }
